5a52964309ee759f3b1c5b7c8cee0aff1cee66ff
Puneeth-Kumar-B <101204256+Puneeth-Kumar-B@users.noreply.github.com>
2023-01-25 18:55:02 +0530

    Update product.validator.js

--- validations/product.validator.js
+++ validations/product.validator.js
@@ -1,25 +1,27 @@
 const joi=require('joi');
 const {bodyParamValidation}=require('./validator');
 
+
+//VALIDATION FOR PRODUCT CREATION
 const createProductValidation=(req, res, next) => {
     const schema=joi.object({
         productName: joi.string().required(),
         category: joi.string().required(),
         brand: joi.string().required(),
         price: joi.number().required(),
-        quantity: joi.number().optional(),
         isDeleted: joi.boolean().optional()
     })
     return bodyParamValidation(req, res, next, schema)
 }
 
+
+//VALIDATION FOR PRODUCT UPDATION
 const updateProductValidation=(req, res, next) => {
     const schema=joi.object({
         productName: joi.string().optional(),
         category: joi.string().optional(),
         brand: joi.string().optional(),
         price: joi.number().optional(),
-        quantity: joi.number().optional(),
         isDeleted: joi.boolean().optional()
     })
     return bodyParamValidation(req, res, next, schema)
